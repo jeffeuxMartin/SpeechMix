7848fc68dfa2ee8a1c87406051cf649f5df53230
陳建成（Chien-Cheng (Jeff) Chen） <43311603+jeffeuxMartin@users.noreply.github.com>
2022-03-07 01:17:59 +0800

    _

--- train.py
+++ train.py
@@ -21,48 +21,6 @@ from transformers import (Trainer,
 import asrp
 import speechmix
 
-
-class FreezingCallback(TrainerCallback):
-    def __init__(self, trainer, freeze_model, freeze_epoch=3):
-        self.trainer = trainer
-        self.freeze_model = freeze_model
-        self.freeze_epoch = freeze_epoch
-        self.current_step_idx = 0
-        self.default_param_fix = {}
-        self.name_list = []
-        for name, param in self.freeze_model.named_parameters():
-            self.name_list.append(name)
-            self.default_param_fix[name] = param.requires_grad
-        self.freeze_layers = int(len(self.default_param_fix.keys()) / freeze_epoch)
-
-    def on_epoch_begin(self, 
-        args: TrainingArguments, 
-        state: TrainerState, 
-        control: TrainerControl, 
-        **kwargs
-    ):
-        if state.epoch < self.freeze_epoch:
-            release = self.name_list[-int(self.freeze_layers * state.epoch):]
-            for name, param in self.freeze_model.named_parameters():
-                if name in release:
-                    param.requires_grad = True
-                else:
-                    param.requires_grad = False
-        else:
-            for name, param in self.freeze_model.named_parameters():
-                param.requires_grad = self.default_param_fix[name]
-        self.current_step_idx += 1
-
-    def on_save(
-        self, 
-        args: TrainingArguments, 
-        state: TrainerState, 
-        control: TrainerControl, 
-        **kwargs
-    ):
-        for name, param in self.trainer.model.named_parameters():
-            param.requires_grad = True
-
 @dataclass
 class DataCollatorWithPadding:
     tokenizer: AutoTokenizer
@@ -113,6 +71,47 @@ class DataCollatorWithPadding:
 
         torch.cuda.empty_cache()
         return batch
+
+class FreezingCallback(TrainerCallback):
+    def __init__(self, trainer, freeze_model, freeze_epoch=3):
+        self.trainer = trainer
+        self.freeze_model = freeze_model
+        self.freeze_epoch = freeze_epoch
+        self.current_step_idx = 0
+        self.default_param_fix = {}
+        self.name_list = []
+        for name, param in self.freeze_model.named_parameters():
+            self.name_list.append(name)
+            self.default_param_fix[name] = param.requires_grad
+        self.freeze_layers = int(len(self.default_param_fix.keys()) / freeze_epoch)
+
+    def on_epoch_begin(self, 
+        args: TrainingArguments, 
+        state: TrainerState, 
+        control: TrainerControl, 
+        **kwargs
+    ):
+        if state.epoch < self.freeze_epoch:
+            release = self.name_list[-int(self.freeze_layers * state.epoch):]
+            for name, param in self.freeze_model.named_parameters():
+                if name in release:
+                    param.requires_grad = True
+                else:
+                    param.requires_grad = False
+        else:
+            for name, param in self.freeze_model.named_parameters():
+                param.requires_grad = self.default_param_fix[name]
+        self.current_step_idx += 1
+
+    def on_save(
+        self, 
+        args: TrainingArguments, 
+        state: TrainerState, 
+        control: TrainerControl, 
+        **kwargs
+    ):
+        for name, param in self.trainer.model.named_parameters():
+            param.requires_grad = True
 
 def compute_metrics(pred):
     global model
